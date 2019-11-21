c1792bcfac6a82a665028aebb2163f40b6b03020
Sergey Eromchenko <40355944+flunk72@users.noreply.github.com>
2019-11-21 19:56:46 +0500

    add table

---
+++ test/src/components/App/App.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+
+import './App.css';
+
+export class App extends Component {
+  state = {
+    goods: [],
+    isName: false,
+    isCount: false
+  }
+    componentDidMount() {
+      fetch('http://localhost:3000/data.json')
+        .then(response => response.json()) 
+        .then(({ dataName }) => this.setState(() => ({
+          goods: dataName,
+            }
+          ))
+        )
+    }
+    const newName
+    render () {
+      return (
+      <div>
+        <div className="sorting-block">
+        Сортировать по:
+          <button className="sorting-btn">Имени</button>
+          <button className="sorting-btn">Дате</button>
+          <button className="sorting-btn">Количеству</button>
+        </div>
+        <div className="table">
+          <div className="container">
+            <div className="table-name">
+              {this.state.goods.map(g =>
+              <div key={g.id}>
+              <span>{g.name}</span>
+            </div>
+            )}
+            </div>
+            <div className="table-date">
+            {this.state.goods.map(g =>
+            <div key={g.id}>
+              <span>{g.date}</span>
+            </div>
+            )}
+            </div>
+            <div className="table-count">
+            {this.state.goods.map(g =>
+            <div key={g.id}>
+              <span>{g.count}</span>
+            </div>
+            )}
+            </div>
+          </div>
+        </div>
+      </div>
+      )
+    }
+}
